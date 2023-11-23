00dd87a96430a0896c76d679ced012334f60e335
어성은 <djandcl@naver.com>
2023-11-23 14:11:48 +0900

    war프로젝트 생성

---
+++ springboot_project/ojoa2/src/main/java/com/ojo/ojoa/OjoaApplication.java
@@ -0,0 +1,13 @@
+package com.ojo.ojoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjoaApplication.class, args);
+	}
+
+}
---
+++ springboot_project/ojoa2/src/test/java/com/ojo/ojoa/OjoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojo.ojoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
